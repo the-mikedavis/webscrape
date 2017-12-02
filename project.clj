964d5de0e477422df028dda934563e1102e988d4
Michael Davis <rubiksmaster65@yahoo.com>
2017-12-02 15:06:13 -0500

    Get a basic working version
    Still needs to filter out nil maps

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject webscrape "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject webscrape "0.1.0"
+  :description "A Basic Webscraping Tool"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]
+                 [http-kit "2.1.18"]
+                 [org.clojure/tools.trace "0.7.9"]]
   :main ^:skip-aot webscrape.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/webscrape/core.clj
+++ src/webscrape/core.clj
@@ -1,7 +1,25 @@
 (ns webscrape.core
+  (:require [net.cgrand.enlive-html :as html]
+            [org.httpkit.client :as http]
+            [clojure.string :as string])
+  (:refer-clojure :exclude [class]) ; don't need class keyword
   (:gen-class))
 
+(defn ^:dynamic url->dom
+  "Gets the DOM of a url's response."
+  [url]
+  (html/html-snippet
+    (:body @(http/get url {:insecure? true}))))
+
+(defn ^:dynamic extract
+  "Extract from dom the tags with classes. Creates a lazy-seq."
+  [dom tag class]
+  (map (comp #(string/join " " %) :content)
+       (html/select dom
+                    [(keyword (str tag "." class))])))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [url tag class]
+  (let [captures (extract (url->dom url)
+                          tag class)]
+    (map println captures)))
