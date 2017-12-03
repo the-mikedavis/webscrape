53dfd979e1dcb3bafa0dfbfd95fc320a38c89441
Michael Davis <rubiksmaster65@yahoo.com>
2017-12-02 20:14:44 -0500

    Add a possibly working serial multi-site option

--- src/webscrape/core.clj
+++ src/webscrape/core.clj
@@ -8,8 +8,9 @@
 (defn parse-csv
   "Read in the values from a csv file"
   [filename]
-  (map #(string/split % #",")
-       (string/split (slurp filename) #"\n")))
+  (remove nil?
+          (map #(string/split % #",")
+               (string/split (slurp filename) #"\n"))))
 
 (defn url->dom
   "Gets the DOM of a url's response."
@@ -18,16 +19,28 @@
     (:body @(http/get url {:insecure? true}))))
 
 (defn extract
-  "Extract from dom the tags with classes. Creates a lazy-seq."
-  [dom tag class]
-  (map (comp #(string/join " " %) :content)
-       (html/select dom
-                    [(keyword (str tag "." class))])))
+  "Extract from dom the tags with classes."
+  ([filename]
+   true)
+  ([url tag class]
+   (map (comp #(string/join " " %) :content)
+        (html/select (url->dom url)
+                     [(keyword (str tag "." class))]))))
+
+(defn print-list
+  "Print out a list line by line."
+  [list-arg]
+  (map println list-arg))
 
 (defn -main
   ([]
    (map println (extract (url->dom "http://news.ycombinator.com/")
                          "a" "storylink")))
+  ([filename]
+   (doseq [visit (parse-csv filename)]
+     (print-list (extract (first visit)
+                         (second visit)
+                         (nth visit 2)))))
   ([url tag class]
-   (map println (extract (url->dom url)
-                         tag class))))
+   (print-list (extract url)
+                        tag class)))
